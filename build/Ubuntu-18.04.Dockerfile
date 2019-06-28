506e336eb16128aa358ca18d3e85d460dbf13edb
Chris Coffey <cmcof273@gmail.com>
2019-06-28 10:35:52 -0400

    Add linux build script

---
+++ build/Ubuntu-18.04.Dockerfile
@@ -0,0 +1,12 @@
+FROM ubuntu:bionic-20190612
+
+RUN apt-get update && apt-get install curl git -y
+
+RUN mkdir binary
+RUN curl -sSL https://get.haskellstack.org/ | sh
+RUN git clone https://github.com/CollegeVine/confcrypt
+
+RUN cd confcrypt && stack install
+VOLUME binary
+
+RUN cp -r "$(stack path --local-bin)/" binary/
---
+++ build/gen_linux.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+# Build the Linux docker image, spin up a container, then copy out the confcrypt binary and
+# dump it locally. Finally, tear down the image.
+
+docker build -t confcrypt-linux -f build/Ubuntu-18.04.Dockerfile .
+id=$(docker create confcrypt-linux)
+docker cp "$id":/root/.local/bin/confcrypt confcrypt
+docker rm "$id"
+tar -czf confcrypt-linux-x64.tar.gz confcrypt
